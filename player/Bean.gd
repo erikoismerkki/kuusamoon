e917ab607986dfed6781430ce800248b9626344f
erikoismerkki <miikka.messo@hotmail.com>
2023-12-30 20:01:29 +0200

    added headphones and fixed hitboxes

--- player/Bean.gd
+++ player/Bean.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
 
 
-var SPEED = 3
-var JUMP_VELOCITY = 6
+const  SPEED = 3
+const JUMP_VELOCITY = 6
+var bonusspeed = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -19,18 +20,18 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY+bonusspeed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("moveL", "moveR", "moveF", "moveB")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * (SPEED+bonusspeed), SPEED/3)
+		velocity.z = move_toward(velocity.z, direction.z * (SPEED+bonusspeed), SPEED/3)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED/2)
-		velocity.z = move_toward(velocity.z, 0, SPEED/2)
+		velocity.x = move_toward(velocity.x, 0, SPEED/3)
+		velocity.z = move_toward(velocity.z, 0, SPEED/3)
 
 	move_and_slide()
 	
@@ -45,11 +46,9 @@ func _input(event):
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if event.is_action_pressed("run"):
-		SPEED = 7
-		JUMP_VELOCITY = 10
+		bonusspeed = 3
 	if event.is_action_released("run"):
-		SPEED = 3
-		JUMP_VELOCITY = 7
+		bonusspeed = 0
 
 func _process(delta):
 	rotate_y(mousex/-1000)
